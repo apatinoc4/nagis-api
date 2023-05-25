20c1561cf0422e03f01ed95df01377af4cd130ec
Andres Patino <apatinoc4@gmail.com>
2023-05-24 22:49:34 -0500

    fix: temporary removal of yaml files

--- scripts/after_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'run after_install.sh: ' >> /home/ubuntu/nagis-api/deploy.log
-
-echo 'cd /home/ubuntu/nagis-api' >> /home/ubuntu/nagis-api/deploy.log
-cd /home/ubuntu/nagis-api >> /home/ubuntu/nagis-api/deploy.log
-
-echo 'npm install' >> /home/ubuntu/nagis-api/deploy.log
-npm install >> /home/ubuntu/nagis-api/deploy.log
--- scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ubuntu/nagis-api/deploy.log
-
-echo 'pm2 restart nagis-api' >> /home/ubuntu/nagis-api/deploy.log
-pm2 restart nagis-api >> /home/ubuntu/nagis-api/deploy.lo
