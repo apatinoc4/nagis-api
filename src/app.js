accaff82c7a1686a42db41bdff5d11abee78b13c
Andres Patino <apatinoc4@gmail.com>
2023-05-26 22:29:23 -0500

    fix: revert https server modifications

--- src/app.js
+++ src/app.js
@@ -1,40 +1,22 @@
 import cors from "cors";
 import express from "express";
-import http from "http";
-import https from "https";
-import fs from "fs";
 import imageRouter from "./routes/wotv/imgRoutes.js";
 import unitsRouter from "./routes/wotv/unitRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
-// Middleware
-app.use(cors());
-app.use(express.static("public"));
-
-// Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.listen(port, () => {
+  console.log(`Nagis-api now listening on port ${port} 🔥🔥`);
+});
+
+app.use(cors());
+
+app.use(express.static("public"));
+
 app.use("/api/wotv/images", imageRouter);
 app.use("/api/wotv/units", unitsRouter);
-
-if (process.env.NODE_ENV === "production") {
-  const httpsOptions = {
-    key: fs.readFileSync("/etc/ssl/private/private.key"),
-    cert: fs.readFileSync("/etc/ssl/certs/certificate.crt"),
-  };
-
-  // Run the HTTPS server
-  https.createServer(httpsOptions, app).listen(port, () => {
-    console.log(`Nagis-api now listening on port ${port} (HTTPS) 🔒🔥`);
-  });
-} else {
-  // Run the HTTP server for development
-  const server = http.createServer(app);
-  server.listen(port, () => {
-    console.log(`Nagis-api now listening on port ${port} (HTTP)`);
-  });
-}
