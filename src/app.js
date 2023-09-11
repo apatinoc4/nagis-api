0ffba00698537ffd4e92625f2414a0454c1addb4
Andres Patino <apatinoc4@gmail.com>
2023-09-10 22:12:49 -0500

    fix: reverting vercel changes

--- src/app.js
+++ src/app.js
@@ -4,16 +4,14 @@ import imageRouter from "./routes/wotv/imgRoutes.js";
 import unitsRouter from "./routes/wotv/unitRoutes.js";
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
 });
 
-app.listen(port, () => {
-  console.log(`Nagis-api now listening on port ${port} 🔥🔥`);
+app.listen(PORT, () => {
+  console.log(`Nagis-api now listening on port ${PORT} 🔥🔥`);
 });
 
 app.use(cors());
